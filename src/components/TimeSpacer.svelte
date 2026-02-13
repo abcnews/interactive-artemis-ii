46cd37a38fda17168cdc1c3903cb725f392caa47
Joshua Byrd <phocks@gmail.com>
2026-02-13 13:36:31 +1000

    spacer style in JS

--- src/components/TimeSpacer.svelte
+++ src/components/TimeSpacer.svelte
@@ -1,34 +1,39 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const spacer = document.querySelector("[data-key='spacer']") as HTMLElement;
+    spacer.style = `--grid-template-rows: minmax(200px, auto) minmax(500px, auto) minmax(300px, auto) minmax(5px, auto)`;
+  });
 </script>
 
 <style lang="scss">
   :global {
-    .Block.is-piecemeal > .Block-content > :first-child {
-      margin-top: 0px !important;
-      margin-bottom: 0px !important;
-    }
-
-    .Block.is-piecemeal > .Block-content:nth-child(2) > p:first-child {
-      margin-top: 0vh !important;
-    }
-
-    .Block-content {
-      min-height: unset !important;
-
-      > p {
-        margin-top: 0 !important;
-        margin-bottom: 0 !important;
+    [data-component="Decoy"] {
+      .Block.is-piecemeal > .Block-content > :first-child {
+        margin-top: 0px !important;
+        margin-bottom: 0px !important;
       }
-    }
 
-    .Block {
-      display: grid;
-      align-items: start;
-      min-height: unset !important;
-      grid-template-rows: minmax(2px, auto) minmax(50px, auto) minmax(2px, auto) minmax(
-          5px,
-          auto
-        );
+      .Block.is-piecemeal > .Block-content:nth-child(2) > p:first-child {
+        margin-top: 0vh !important;
+      }
+
+      .Block-content {
+        min-height: unset !important;
+
+        > p {
+          margin-top: 0 !important;
+          margin-bottom: 0 !important;
+        }
+      }
+
+      .Block {
+        display: grid;
+        align-items: start;
+        min-height: unset !important;
+        grid-template-rows: var(--grid-template-rows);
+      }
     }
   }
 </style>
