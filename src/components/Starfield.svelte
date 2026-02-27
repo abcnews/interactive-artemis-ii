8eed5a656fbfbc2d34829a3e4b7d0d318ad74d1b
Joshua Byrd <phocks@gmail.com>
2026-02-27 15:04:20 +1000

    Use seeded random

--- src/components/Starfield.svelte
+++ src/components/Starfield.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { T } from "@threlte/core";
   import * as THREE from "three";
+  import Rand, { PRNG } from 'rand-seed';
+
+  const rand = new Rand('artemis-ii');
 </script>
 
 <!-- TODO: only render necessary stars -->
@@ -10,7 +13,7 @@
       const count = 100000;
       const pos = new Float32Array(count * 3);
       for (let i = 0; i < count * 3; i++) {
-        pos[i] = (Math.random() - 0.5) * 5000;
+        pos[i] = (rand.next() - 0.5) * 5000;
       }
       geo.setAttribute("position", new THREE.BufferAttribute(pos, 3));
     }}
