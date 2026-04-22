778efcb73ea25a34c9e4539000faaa9a54b892a9
Joshua Byrd <phocks@gmail.com>
2026-04-22 14:15:20 +1000

    IIFE the loader

--- public/loader_dev.js
+++ public/loader_dev.js
@@ -1,5 +1,7 @@
-console.log("Loading local dev module script...");
-const script = document.createElement("script");
-script.type = "module";
-script.src = "http://localhost:5173/src/main.ts";
-document.body.appendChild(script);
+(function () {
+  console.log("Loading local dev module script...");
+  const script = document.createElement("script");
+  script.type = "module";
+  script.src = "http://localhost:5173/src/main.ts";
+  document.body.appendChild(script);
+})();
